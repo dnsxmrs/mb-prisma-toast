74c584d759a2182301628617b1c2355e87645448
dnsxmrs <ericemarial@gmail.com>
2025-04-18 00:49:58 +0800

    feat: implement session management with JWT for authentication

---
+++ src/lib/session.ts
@@ -0,0 +1,47 @@
+import "server-only";
+import { SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+
+const secretKey = process.env.JWT_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+
+
+export async function createSession(userId: string) {
+    const expiresAt = new Date(Date.now() + 1 * 60 * 60 * 1000);
+    const session = await encrypt({ userId, expiresAt });
+
+    (await cookies()).set("session", session, {
+        httpOnly: true,
+        secure: true,
+        expires: expiresAt,
+    });
+}
+
+type SessionPayload = {
+    userId: string;
+    expiresAt: Date;
+}
+
+export async function encrypt(payload: SessionPayload) {
+    return new SignJWT(payload)
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime("1h")
+        .sign(encodedKey);
+}
+
+export async function deleteSession() {
+    (await cookies()).delete("session");
+}
+
+export async function decrypt(session: string | undefined = "") {
+    try {
+        const { payload } = await jwtVerify(session, encodedKey, {
+            algorithms: ["HS256"],
+        });
+        return payload;
+    } catch (e) {
+        console.log("Failed to verify session token");
+    }
+}
---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { decrypt } from "./lib/session";
+import { NextRequest, NextResponse } from "next/server";
+
+const protectedRoutes = ['/dashboard', '/settings', '/profile'];
+const publicRoutes = ['/sign-in', '/sign-up', '/forgot-password'];
+
+export default async function middleware(req: NextRequest) {
+    const path = req.nextUrl.pathname;
+    const isProtectedRoute = protectedRoutes.includes(path);
+    const isPublicRoute = publicRoutes.includes(path);
+
+    const cookie = req.cookies.get('session')?.value;
+
+    const session = await decrypt(cookie);
+
+    if (isProtectedRoute && !session?.userId) {
+        return NextResponse.redirect(new URL("/", req.nextUrl));
+    }
+
+    if (isPublicRoute && session?.userId) {
+        return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+    }
+
+    return NextResponse.next();
+}
